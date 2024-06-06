529ec0d0d49c69d212ebeb7f8ead9203675b1433
Jerry D Farrell <14806267+jfarrell423@users.noreply.github.com>
2024-06-06 10:52:46 -0400

    Adding execution policy script

---
+++ Set-Execution-Policy.ps1
@@ -0,0 +1,12 @@
+# powershell execution policies
+# Warning you need to understand what each policy allows.
+# For more information read here from Microsofts learn Page.
+# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_execution_policies?view=powershell-7.4
+#
+Set-ExecutionPolicy RemoteSigned
+# Set-ExecutionPolicy AllSigned
+# Set-ExecutionPolicy Bypass
+# Set-ExecutionPolicy Default
+# Set-ExecutionPolicy Restricted
+# Set-ExecutionPolicy Undefined
+# Set-ExecutionPolicy Unrestricted
